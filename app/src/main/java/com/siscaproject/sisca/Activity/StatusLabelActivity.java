91db6bbc6ed4e843d7236aaa2a23a1f64b48532b
Muhammad Marchell <marchellehcram@gmail.com>
2019-01-28 13:00:07 +0700

    January 28th Update
    - Created New Supplier Form
    - Created Model Assets Activity
    - Created Status Label Activity
    - Created Splash Screen Activity
    -

---
+++ app/src/main/java/com/siscaproject/sisca/Activity/StatusLabelActivity.java
@@ -0,0 +1,15 @@
+package com.siscaproject.sisca.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.siscaproject.sisca.R;
+
+public class StatusLabelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status_label);
+    }
+}
