bcc8e1c5b3d655b71e580e6d11b4fd8353177403
Muhammad Marchell <marchellehcram@gmail.com>
2019-06-20 12:45:47 +0700

    June 20th Update

--- app/src/main/java/com/siscaproject/sisca/Fragment/ScanFragment.java
+++ app/src/main/java/com/siscaproject/sisca/Fragment/ScanFragment.java
@@ -2,11 +2,13 @@ package com.siscaproject.sisca.Fragment;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,19 +17,27 @@ import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.GravityEnum;
 import com.afollestad.materialdialogs.MaterialDialog;
-import com.siscaproject.sisca.Model.AssetModel;
+import com.siscaproject.sisca.Model.AssetMutasi;
 import com.siscaproject.sisca.Model.PairedDevice;
+import com.siscaproject.sisca.Model.ResponseIndex;
 import com.siscaproject.sisca.R;
 import com.siscaproject.sisca.Utilities.APIProperties;
 import com.siscaproject.sisca.Utilities.BluetoothConnector;
+import com.siscaproject.sisca.Utilities.Header;
 import com.siscaproject.sisca.Utilities.UserService;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ScanFragment extends Fragment{
     private static final String TAG = "ScanFragment";
@@ -55,7 +65,7 @@ public class ScanFragment extends Fragment{
     private ListView mResultsListView;
 
     private ArrayList<String> data = new ArrayList<>();
-    private ArrayList<AssetModel> listAsset;
+    private ArrayList<AssetMutasi> listAsset;
 
     public ScanFragment() {
         // Required empty public constructor
@@ -66,25 +76,25 @@ public class ScanFragment extends Fragment{
         super.onCreate(savedInstanceState);
 
         userService = APIProperties.getUserService();
-        //getAsset();
+        getAsset();
     }
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scan, container, false);
         ButterKnife.bind(this, view);
 
         mResultsArrayAdapter = new ArrayAdapter<>(ScanFragment.this.getContext(), android.R.layout.simple_list_item_1);
 
         mResultsArrayAdapter.add("0000");
-        mResultsArrayAdapter.add("81016");
+        mResultsArrayAdapter.add("7163");
 
         mResultsListView = view.findViewById(R.id.lv_data);
         mResultsListView.setAdapter(mResultsArrayAdapter);
         mResultsListView.setFastScrollEnabled(true);
+
+        totalData.setText(Integer.toString(mResultsArrayAdapter.getCount()) + " items detected");
 
         mResultsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -95,6 +105,14 @@ public class ScanFragment extends Fragment{
                 //isAssetExist(mResultsArrayAdapter.getItem(position));
                 //showCreateDialog(mResultsArrayAdapter.getItem(position));
                 //showDialog(mResultsArrayAdapter.getItem(position));
+                try{
+                    String s = mResultsArrayAdapter.getItem(position);
+                    showDialog(s);
+                    Log.i(TAG, "onItemClick: " + s);
+                }catch(Exception e){
+                    Log.e(TAG, "onItemClick: " + e.getMessage() );
+                    showToast("Error occurred");
+                }
             }
         });
 
@@ -131,7 +149,7 @@ public class ScanFragment extends Fragment{
         });
     }
 
-    /*
+
     private void showCreateDialog(final String tag){
         Log.i(TAG, "showCreateDialog: called");
 
@@ -144,9 +162,11 @@ public class ScanFragment extends Fragment{
                 .onPositive(new MaterialDialog.SingleButtonCallback() {
                     @Override
                     public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        /*
                         Intent intent = new Intent(getActivity(), FormNewAssetActivity.class);
                         intent.putExtra("asset_tag", tag);
                         startActivity(intent);
+                        */
                     }
                 })
                 .onNegative(new MaterialDialog.SingleButtonCallback() {
@@ -162,11 +182,12 @@ public class ScanFragment extends Fragment{
     }
 
 
-    private void showInfoDialog(final AssetModel asset){
+    private void showInfoDialog(final AssetMutasi asset){
         Log.i(TAG, "showInfoDialog: called");
 
         String name = "Name : " + asset.getName();
-        String tag = "AssetModel Tag : " + asset.getAsset_id();
+        //String tag = "AssetModel Tag : " + asset.getAsset_id();
+        String tag = "AssetModel Tag : " + asset.getAsset_rfid();
 
         MaterialDialog.Builder builder = new MaterialDialog.Builder(getContext())
                 .title("AssetModel Registered!")
@@ -179,12 +200,14 @@ public class ScanFragment extends Fragment{
                     @Override
                     public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
                         try {
+                            /*
                             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                             ShowAssetFragment fragment = new ShowAssetFragment();
                             fragment.setAsset(asset);
                             FragmentTransaction transaction = fragmentManager.beginTransaction();
                             transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
                             transaction.add(android.R.id.content, fragment).addToBackStack(null).commit();
+                            */
                         }catch(Exception e){
                             Log.e(TAG, "onClick: " + e.getMessage() );
                         }
@@ -196,54 +219,61 @@ public class ScanFragment extends Fragment{
         infoDialog.show();
     }
 
-
-
-    private AssetModel isAssetExist(String assetTag){
-        for(AssetModel a : listAsset){
+    private Boolean isAssetExist(String assetTag){
+        for(AssetMutasi a : listAsset){
             String tag = a.getAsset_id();
             if (tag.equals(assetTag)){
                 Log.i(TAG, "isAssetExist: true");
-                return a;
+                return true;
             }
         }
         Log.i(TAG, "isAssetExist: false");
-        return null;
+        return false;
     }
 
-
     private void showDialog(String tag){
-        AssetModel a = isAssetExist(tag);
+        Boolean a = isAssetExist(tag);
 
-        if(a != null){
+        if(a.equals(true)){
             //showInfoDialog(a);
+            showToast("true");
         }
         else{
             //showCreateDialog(tag);
+            showToast("false");
         }
     }
 
 
     private void getAsset() {
-        Call<ResponseIndex<AssetModel>> call = userService.indexFixed(Header.auth, Header.accept);
-        call.enqueue(new Callback<ResponseIndex<AssetModel>>() {
+        Call<ResponseIndex<AssetMutasi>> call = userService.indexAssetMutasi(Header.auth, Header.accept);
+        call.enqueue(new Callback<ResponseIndex<AssetMutasi>>() {
             @Override
-            public void onResponse(Call<ResponseIndex<AssetModel>> call, Response<ResponseIndex<AssetModel>> response) {
-                if (response.isSuccessful()) {
-                    int total = response.body().getTotal();
-                    Log.i(TAG, "onResponse: total " + total);
-
-                    listAsset = response.body().getRows();
-                } else {
-                    Log.i(TAG, "onResponse: else");
+            public void onResponse(Call<ResponseIndex<AssetMutasi>> call, Response<ResponseIndex<AssetMutasi>> response) {
+                if(response.isSuccessful()){
+                    Log.i(TAG, "onResponse: total " + response.body().getTotal());
+                    listAsset = response.body().getData();
+                    Log.i(TAG, "onResponse: listAsset size " + listAsset.size());
+                    Log.i(TAG, "onResponse: listAsset rfid random index == " + listAsset.get(4).getAsset_rfid());
+                    Log.i(TAG, "onResponse: listAsset assetid random index == " + listAsset.get(4).getAsset_id());
+                    Log.i(TAG, "onResponse: listAsset name random index == " + listAsset.get(4).getName());
+                }
+                else{
+                    Log.e(TAG, "getAsset onResponse: not successful");
+                    showToast("Unable to get Asset data");
                 }
             }
 
             @Override
-            public void onFailure(Call<ResponseIndex<AssetModel>> call, Throwable t) {
-                Log.e(TAG, "onFailure: " + t.getMessage());
+            public void onFailure(Call<ResponseIndex<AssetMutasi>> call, Throwable t) {
+                Log.e(TAG, "getAsset onFailure: " + t.getMessage() );
+                showToast("Unable to get Asset data");
             }
         });
     }
 
-    */
+    private void showToast(String message){
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
+
 }
